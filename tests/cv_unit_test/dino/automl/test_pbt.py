c66e35407bd9ccd5560c811eea4d3c1cd8ceb32b
Ramanathan Arunachalam <rarunachalam@nvidia.com>
2026-08-10 04:14:48 +0000

    Add DINO AutoML SPDX headers
    Signed-off-by: Ramanathan Arunachalam <rarunachalam@nvidia.com>

--- tests/cv_unit_test/dino/automl/test_pbt.py
+++ tests/cv_unit_test/dino/automl/test_pbt.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
 # Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
